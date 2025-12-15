092bfa003640b0297b52689e0cb6e6e5a1c18d62
Rohit_More <morer4851@gmail.com>
2025-12-15 17:09:59 +0530

    Triggered basic message when object was detected

---
+++ emailing.py
@@ -0,0 +1,5 @@
+def send_email():
+	print("E-mail sent.")
+	
+if __name__ == "__main__":
+	send_email()
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import time
 import cv2
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
-first_frame = None # Initial frame to compare with other frames
+first_frame = None
+status_list = []
+
 while True:
+	status = 0
 	check, frame = video.read()
 	
 	gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -16,6 +20,7 @@ while True:
 	
 	delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 	thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+	
 	dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
 	cv2.imshow("My video", dil_frame)
 	
@@ -26,8 +31,17 @@ while True:
 			continue
 			
 		x, y, w, h = cv2.boundingRect(contour)
-		
-		cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+		rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+		if rectangle.any(): # If moving object occurs
+			status = 1
+			
+	status_list.append(status) # Checks for moving object entering(first 1) and exiting(0 after 1) frame
+	status_list = status_list[-2:] # Last two element to check
+	
+	if status_list[0] == 1 and status_list[1] == 0:
+		send_email()
+	
+	print(status_list)
 		
 	cv2.imshow("Video", frame)
 	key = cv2.waitKey(1)
